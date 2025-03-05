42823fabebf2ecb6fc6b1959f6dc77cbeeb9f713
kitkataleena <164939676+kitkataleena@users.noreply.github.com>
2025-03-04 21:05:28 -0600

    read and check seq

--- align sequences.py
+++ align sequences.py
@@ -4,4 +4,10 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-sequences = list(SeqIO.parse("sequences.fasta"))
+seq1_file = "sequences/QHU36824.1.fasta"
+seq2_file = "seqeuence/YP_009724390.1(refseq).fasta"
+
+seq1 = str(SeqIO.read(seq1_file, "fasta").seq)
+seq2 = str(SeqIO.read(seq2_file, "fasta").seq)
+
+print(seq1)
